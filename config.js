56750b7d12d005203ac2c56caad161fba134118f
Oussama choumoul <choumouloussama@gmail.com>
2021-05-18 06:21:49 +0100

    Dev code version 1.0

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    user : 'uoe4tzdvxdkj5jbbpowm17xfetc42m',
+    token : 'af6nb5v83wm3ri93hpuw6z9xti2axo',
+    CREDS : {
+        username: 'choumouloussama@gmail.com',
+        password: 'Gr33k01#2020@'
+    },
+    status: true,
+    checkURL: 'https://candidature.1337.ma/piscines',
+    checkPhrase : "Aucune piscine n'est actuellement disponible. Pour être au courant dès qu'une nouvelle piscine s'ouvrira, tu peux nous follow sur twitter :"
+}
